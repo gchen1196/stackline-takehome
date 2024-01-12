24bbf35be02d4182c5ebb542ae3804c895188345
George Chen <gchen@zoiclabs.com>
2024-01-12 10:40:44 -0800

    add logo in top nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import logo from "./assets/stackline_logo.svg";
+import Line from "./components/LineLogo";
 
 function App() {
   return (
     <div className="app-container">
-      <header className="top-nav">Top Navigation</header>
+      <header className="top-nav">
+        <div style={{ display: "flex", flexDirection: "column", width: 200, height: 50 }}>
+          <img src={logo} style={{}} />
+          <Line />
+        </div>
+      </header>
 
       <div className="main-container">
         <aside className="product-container">Product description here</aside>
